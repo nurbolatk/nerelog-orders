7a150b2c0d2e3e025e7304e15af949bce008c3d5
nurbolatk <nurbolattk@gmail.com>
2022-01-25 02:44:48 +0600

    Create useAsync hook

--- src/app/App.js
+++ src/app/App.js
@@ -1,15 +1,16 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
 import './App.css'
 import { filterData } from 'filterData/workerized-filter-data'
+import { useAsync } from 'shared/utils'
 
 function App() {
-  const [orders, setOrders] = useState([])
+  const { data, run } = useAsync()
+
   useEffect(() => {
-    filterData().then((data) => {
-      setOrders(data)
-    })
-  }, [])
-  console.log('app.jsx', orders)
+    run(filterData())
+  }, [run])
+
+  console.log('app.jsx', data.orders)
   return <div className="App">hello</div>
 }
 
