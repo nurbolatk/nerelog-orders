ebc6d34a024a9daf54d1b4022052dbc04b437b56
nurbolatk <nurbolattk@gmail.com>
2022-01-31 03:43:41 +0600

    Adapt to mobile

--- src/app/App.js
+++ src/app/App.js
@@ -4,9 +4,8 @@ import { filterData } from 'webWorker/filterData/workerizedFilterData'
 import { useAsync } from 'utils'
 import { List } from 'components/molecules'
 import { Map } from 'components/organisms'
-import { useInputValue } from './hooks'
+import { useInputValue, useVirtualList } from './hooks'
 import { SearchInput } from 'components/atoms'
-import { useVirtual } from 'react-virtual'
 
 function App() {
   const mapRef = React.useRef()
@@ -15,22 +14,14 @@ function App() {
     data: [],
     status: 'pending',
   })
-  const listRef = React.useRef()
 
-  const rowVirtualizer = useVirtual({
-    size: data.length,
-    parentRef: listRef,
-    estimateSize: React.useCallback(() => 232, []),
-    overscan: 10,
-  })
+  const [listRef, rowVirtualizer] = useVirtualList(data)
   const [inputValue, handleInputChange, clearInputValue] = useInputValue()
   const [selectedItem, setSelectedItem] = React.useState(null)
 
   useEffect(() => {
     run(filterData(inputValue))
   }, [inputValue, run])
-
-  console.log('data', inputValue)
 
   const handleMarkerClick = useCallback(
     (order, index) => {
@@ -46,9 +37,9 @@ function App() {
   }, [])
 
   return (
-    <main className="p-4">
-      <div className="card max-w-7xl mx-auto grid grid-cols-3 gap-8 overflow-hidden">
-        <div className="col-span-1">
+    <main className="lg:p-4">
+      <div className="card max-w-7xl mx-auto grid lg:grid-cols-3 grid-cols-1 grid-rows-2 lg:grid-rows-1 gap-8 overflow-hidden">
+        <div className="col-span-1 col-start-1">
           <SearchInput
             className="relative mb-4"
             value={inputValue}
@@ -63,10 +54,10 @@ function App() {
             onItemClick={handleItemClick}
             virtualRows={rowVirtualizer.virtualItems}
             totalHeight={rowVirtualizer.totalSize}
-            className="relative overflow-y-auto h-[80vh]"
+            className="relative overflow-y-auto h-[40vh] lg:h-[80vh]"
           />
         </div>
-        <div className="relative col-span-2 -m-4 overflow-hidden">
+        <div className="relative lg:col-span-2 lg:col-start-2 -m-4 overflow-hidden row-start-1 row-end-2">
           <Map markers={data} ref={mapRef} onMarkerClick={handleMarkerClick} />
         </div>
       </div>
--- src/components/atoms/ListItem.js
+++ src/components/atoms/ListItem.js
@@ -10,17 +10,17 @@ export function ListItem({ item, isSelected, onClick, measureRef, ...props }) {
       <div ref={measureRef} className="pb-2">
         <button
           onClick={handleItemClick}
-          className={`block text-left w-full border border-slate-200 p-4 rounded-md hover:cursor-pointer
+          className={`block text-left w-full border border-slate-200 p-2 lg:p-4 rounded-md hover:cursor-pointer
            ${isSelected ? 'border-blue-500' : ''}`}>
           <p className="text-secondary">
             Order No <span className="font-bold">${item.id}</span>
           </p>
-          <hr className={cn('my-4', { 'border-blue-500': isSelected })} />
+          <hr className={cn('my-2 lg:my-4', { 'border-blue-500': isSelected })} />
           <div className="grid grid-cols-3">
             <p className="text-zinc-600 col-span-1">Client:</p>
             <p className="col-span-2">{item.client.name}</p>
             <p className="text-zinc-600 col-span-1">Type:</p>
-            <p className="capitalize col-span-2">
+            <p className="capitalize col-span-2 flex items-center gap-x-2 lg:block">
               {item.type}
               {item.isPickup ? (
                 <PickupIcon className="text-green-500" />
@@ -29,7 +29,7 @@ export function ListItem({ item, isSelected, onClick, measureRef, ...props }) {
               )}
             </p>
           </div>
-          <hr className={cn('my-4', { 'border-blue-500': isSelected })} />
+          <hr className={cn('my-2 lg:my-4', { 'border-blue-500': isSelected })} />
 
           <div className="grid grid-cols-3">
             <p>Total:</p>
